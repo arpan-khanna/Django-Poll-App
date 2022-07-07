b2bfa76fb0b5cc5485119d2ab670eae5cde91682
Arpan <arpankhanna70@gmail.com>
2022-07-07 21:00:38 +0530

    deb false

--- Poller/settings.py
+++ Poller/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g6lyol8t^c!k36p&vpb9udyt(4=q=cp3f9q_-a$%)b_#0!y751'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
